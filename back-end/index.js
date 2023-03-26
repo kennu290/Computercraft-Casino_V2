00f67ddbc5ffa85eebc7410bc614a0d40f26a849
KennuByte <73755588+kennu290@users.noreply.github.com>
2023-03-26 11:34:57 +0300

    html.... stuff..

--- back-end/index.js
+++ back-end/index.js
@@ -1,27 +1,45 @@
 var port = process.env.port || 80;
 var express = require('express');
 var handlebars = require('express3-handlebars');
+var bodyParser = require('body-parser');
 var path = require('path');
+const { v4: uuidv4 } = require('uuid');
 var application = express();
 
-var userCount = 5
-var winCount = 50
-var loseCount = 50
-var alerts = []
+var userCount = 5;
+var winCount = 50;
+var loseCount = 50;
+var alerts = [];
+var users = []
 
 application.use(express.static(path.join(__dirname, 'public')));
+application.use(bodyParser.json()); // add this line to parse JSON request body
 
 application.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 
 application.get('/api/data', function(req, res) {
-    var data = {
-      userCount: userCount,
-      wins: winCount,
-      losses: loseCount,
-      alerts: alerts
-    };
-    res.status(200).send(data);
+  var data = {
+    userCount: userCount,
+    wins: winCount,
+    losses: loseCount,
+    alerts: alerts
+  };
+  res.status(200).send(data);
+});
+
+application.post('/api/user', function(req, res) {
+  console.log("Received user creation!");
+  newUserUuid = uuidv4();
+  var user = {
+    Token: newUserUuid,
+    balance: 0,
+    arrayPosition: users.length
+  };
+  res.status(200).send({ data: newUserUuid + "." + users.length });
+  users[users.length] = user
+  userCount++;
+  console.log(users)
 });
 
 application.listen(port);
-console.log("Running site on port " + port)
+console.log("Running site on port " + port);
--- back-end/public/index.js
+++ back-end/public/index.js
@@ -12,7 +12,7 @@ window.onload = function() {
 
 
 function reloadData(){
-    console.log("Running info update!")
+    //console.log(`${new Date().toLocaleString()} - Running info update!`);
     fetch("/api/data")
     .then(response => response.json())
     .then(data => {
@@ -21,23 +21,39 @@ function reloadData(){
         losses = data.losses
         alerts = data.alerts
         winRate = ((wins / (wins + losses)) * 100).toFixed(1);
-        document.getElementById("userCountElement").textContent = userCount
-        document.getElementById("winElement").textContent = wins
-        document.getElementById("lossElement").textContent = losses
-        document.getElementById("winRateElement").textContent = winRate + "%"
-        if (alerts.length != 0){
-            document.getElementById("alertElement").textContent = alerts[0]
-            currentAlertIndex = 0
-        }else{
-            document.getElementById("alertElement").textContent = "No alerts!"
-        }
+        try {
+            document.getElementById("userCountElement").textContent = userCount
+            document.getElementById("winElement").textContent = wins
+            document.getElementById("lossElement").textContent = losses
+            document.getElementById("winRateElement").textContent = winRate + "%"
+            if (alerts.length != 0){
+                document.getElementById("alertElement").textContent = alerts[0]
+                currentAlertIndex = 0
+            }else{
+                document.getElementById("alertElement").textContent = "No alerts!"
+            }
+        }catch (error) {
+            if (error instanceof TypeError) {
+              // Handle the specific error here
+            } else {
+              throw error; // Rethrow the error to let it propagate
+            }
+          }
     })
     .catch(error => console.error(error));
 }
 
 function changeAlertText() {
     if (alerts.length != 0) {
-        document.getElementById("alertElement").textContent = alerts[currentAlertIndex]; // set the text of the element
+        try {
+            document.getElementById("alertElement").textContent = alerts[currentAlertIndex]; // set the text of the element
+        } catch (error) {
+            if (error instanceof TypeError) {
+              // Handle the specific error here
+            } else {
+              throw error; // Rethrow the error to let it propagate
+            }
+          }
         currentAlertIndex++
         if (alerts.length == currentAlertIndex){
             currentAlertIndex = 0
@@ -45,6 +61,32 @@ function changeAlertText() {
     }
 }
 
+function dashboard(){
+    window.location.href = './'
+}
+
+function users(){
+    window.location.href = './users.html'
+}
+
+function machines(){
+    window.location.href = './machines.html'
+}
+
+function payments(){
+    window.location.href = './payment.html'
+}
+
+function settings(){
+    window.location.href = './settings.html'
+}
+
+
+function refreshList(){
+    console.log(window.location.pathname);
+}
+
+
 setInterval(changeAlertText, 5000);
-setInterval(reloadData, 60000);
+setInterval(reloadData, 30000);
 console.log("Intervals on!")
