3b1444b3fb24f4f049eb56eb62ac0b13c5bdcfb9
KennuByte <73755588+kennu290@users.noreply.github.com>
2023-07-20 04:27:10 +0300

    so much stuff. + logging

--- back-end/index.js
+++ back-end/index.js
@@ -20,10 +20,61 @@ var machinePairing = false;
 
 restoreData()
 
+// Setup for logging on startup
+const logFilePath = 'log.txt';
+
+fs.access(logFilePath, fs.constants.F_OK, (err) => {
+  if (err) {
+    // If the file doesn't exist, create an empty file
+    fs.writeFile(logFilePath, '', (err) => {
+      if (err) {
+        console.error('Error creating log.txt:', err);
+      } else {
+        console.log('log.txt created successfully.');
+      }
+    });
+  } else {
+    // If the file already exists, truncate it to make it empty
+    fs.truncate(logFilePath, 0, (err) => {
+      if (err) {
+        console.error('Error truncating log.txt:', err);
+      }
+    });
+  }
+});
+
 application.use(express.static(path.join(__dirname, 'public')));
 application.use(bodyParser.json()); // add this line to parse JSON request body
 
 application.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+
+function getCurrentTimestamp() {
+  const date = new Date();
+  const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZone: 'Europe/Tallinn', // Set the timezone to Estonia (EET)
+  };
+
+  return date.toLocaleString('en-GB', options);
+}
+
+function writeToLog(message) {
+  const logFilePath = 'log.txt';
+  const timestamp = getCurrentTimestamp();
+  const logMessage = `[${timestamp}] ${message}\n`;
+
+  fs.appendFile(logFilePath, logMessage, (err) => {
+    if (err) {
+      console.error('Error writing to log.txt:', err);
+    }
+  });
+}
+
 
 function generateCard(gameType) {
   if (gameType === "blackjack") {
@@ -85,6 +136,7 @@ function findGameByUserToken(token) {
 function startMachinePairing() {
   console.log("Machine pairing started..")
   machinePairing = true;
+  writeToLog('Machine Pairing Started');
   // Set a timer to turn off machine pairing after 3 minutes of inactivity
   pairingTimer = setTimeout(() => {
     endMachinePairing();
@@ -93,6 +145,7 @@ function startMachinePairing() {
 
 function endMachinePairing() {
   console.log("Machine pairing ended..")
+  writeToLog('Machine Pairing Ended');
   machinePairing = false;
   clearTimeout(pairingTimer);
   pairingTimer = null;
@@ -126,8 +179,6 @@ function saveData() {
     if (err) {
       console.error(err);
       return;
-    }else{
-      console.log('Data saved to file!');
     }
   });
 }
@@ -175,15 +226,20 @@ application.get('/api/data/users', function(req, res) {
   // Get size and position from query parameters, with default values if not provided
   const size = parseInt(req.query.size) || 10;
   const position = parseInt(req.query.position) || 0;
+  const hash = req.query.hash || "";
 
-  const data = users.slice(position, position + (size + 1)).map(user => {
-    return {
-      token: user.token,
-      arrayPosition: user.arrayPosition
-    };
-  });
+  if (verifyHash(hash) || findMachineByToken(hash)){
+    const data = users.slice(position, position + (size + 1)).map(user => {
+      return {
+        token: user.token,
+        arrayPosition: user.arrayPosition
+      };
+    });
 
-  res.status(200).send(data);
+    res.status(200).send(data);
+  }else {
+    res.status(401).send("Unauthorized");
+  }
 });
 
 
@@ -191,17 +247,21 @@ application.get('/api/data/machines', function(req, res) {
   // Get size and position from query parameters, with default values if not provided
   const size = parseInt(req.query.size) || 10;
   const position = parseInt(req.query.position) || 0;
+  const hash = req.query.hash || "";
+  if (verifyHash(hash) || findMachineByToken(hash)){
+    const data = machines.slice(position, position + (size + 1)).map(machine => {
+      return {
+        token: machine.token,
+        arrayPosition: machine.arrayPosition
+      };
+    });
 
-  const data = machines.slice(position, position + (size + 1)).map(machine => {
-    return {
-      token: machine.token,
-      arrayPosition: machine.arrayPosition
-    };
-  });
-
-  res.status(200).send(data);
+    res.status(200).send(data);
+  }else
+  {
+    res.status(401).send("Unauthorized");
+  }
 });
-
 
 
 application.post('/api/user/exitGame', function(req, res) {
@@ -216,6 +276,7 @@ application.post('/api/user/exitGame', function(req, res) {
       // Update machine state and send response
       console.log("Game ended for player: " + data.token)
       console.log(req.body)
+      writeToLog('Game ended for player ' + data.token + ' With data: ' + JSON.stringify(req.body) );
       res.status(200).send();
     }
   } else {
@@ -224,24 +285,47 @@ application.post('/api/user/exitGame', function(req, res) {
 });
 
 
-
-
-application.get('/api/user/getUser', function(req, res) {
+application.get('/api/machine/getMachine', function(req, res) {
   // Get size and position from query parameters, with default values if not provided
   const id = parseInt(req.query.id);
   const hash = req.query.hash;
-  if (verifyHash(hash) || findMachineByToken(hash)){
-    if (users[id] !== undefined){
-      if (findMachineByToken(hash)){
-        users[id].lastGameAt = new Date().toISOString().slice(0, 16);
-      }
-      const data = users[id]
+  if (verifyHash(hash)){
+    if (machines[id] !== undefined){
+      
+      const data = machines[id]
       res.status(200).send(JSON.stringify(data));
     }else{
       res.status(404).send()
     }
   }else{
     res.status(401).send("401 Unauthorized")
+  }
+});
+
+
+application.get('/api/user/getUser', function(req, res) {
+  // Get ID and hash from query parameters, with default values if not provided
+  const id = req.query.id || '';
+  const hash = req.query.hash;
+
+  if (verifyHash(hash) || findMachineByToken(hash)) {
+    const user = findUserByToken(id)
+
+    if (user) {
+      if (findMachineByToken(hash)) {
+        user.lastGameAt = new Date().toISOString().slice(0, 16);
+      }
+
+      const data = user;
+      res.status(200).send(JSON.stringify(data));
+      writeToLog(`User "${id}"'s Data was accessed by ${hash}`);
+    } else {
+      res.status(404).send();
+      writeToLog(`${hash} Tried to access a user's data that doesnt exist`);
+    }
+  } else {
+    res.status(401).send("401 Unauthorized");
+    writeToLog(`Unauthorized access attempt by ${hash} for player "${id}"'s data`);
   }
 });
 
@@ -271,16 +355,17 @@ application.delete('/api/user/delete/:id', function(req, res) {
   }
   
   // Check if the user exists
-  if (users[id] === undefined) {
+  if (findUserByToken(id) === undefined) {
     return res.status(404).send({});
   }
 
-  const deletedUser = users[id];
+  arrayPos = findUserByToken(id).arrayPosition
+  const deletedUser = users[arrayPos];
   const lastUser = users[users.length - 1];
   
   // Swap the deleted user with the last user in the array
-  users[id] = lastUser;
-  lastUser.arrayPosition = Number(id);
+  users[arrayPos] = lastUser;
+  lastUser.arrayPosition = Number(arrayPos);
   users.pop();
   
   const data = {
@@ -288,6 +373,7 @@ application.delete('/api/user/delete/:id', function(req, res) {
   };
   
   userCount--;
+  writeToLog(`User "${id}" was Deleted. Data: ${JSON.stringify(deletedUser)}`);
   return res.status(200).send(data);
 });
 
@@ -303,7 +389,7 @@ application.delete('/api/machine/delete/:id', function(req, res) {
   }
   
   // Check if the user exists
-  if (users[id] === undefined) {
+  if (machines[id] === undefined) {
     return res.status(404).send({});
   }
 
@@ -320,8 +406,10 @@ application.delete('/api/machine/delete/:id', function(req, res) {
   };
   
   userCount--;
+  writeToLog(`Machine "${id}" was Deleted. Data: ${JSON.stringify(deletedMachine)}`);
   return res.status(200).send(data);
 });
+
 
 application.post('/api/alerts', function(req, res) {
   console.log("Received an alert:");
@@ -332,10 +420,10 @@ application.post('/api/alerts', function(req, res) {
   } else {
     console.log(alert);
     alerts.push(alert);
+    writeToLog(`Alert was sent: ${alert}`);
     res.status(200).send({});
   }
 });
-
 
 
 application.put('/api/user/update', function(req, res) {
@@ -358,13 +446,36 @@ application.put('/api/user/update', function(req, res) {
   }
 
   if (!verifyHash(hash)) {
+    writeToLog(`Unauthorized attempt to update user ${data.token} With data: ${JSON.stringify(data)}`);
     return res.status(401).send('Unauthorized.');
   }
 
+  oldData = users[data.arrayPosition]
   users[data.arrayPosition] = data;
+  writeToLog(`Successfully updated user "${data.token}" To ${JSON.stringify(data)} From ${JSON.stringify(oldData)}`);
   res.status(200).send({});
 });
 
+
+application.post('/api/user/disable', function(req, res) {
+  const hash = req.body.hash;
+  const userToken = req.body.token;
+  if (findMachineByToken(hash)) {
+    // Authentication successful, process the request
+    if (userToken != null && findUserByToken(userToken)){
+      user = findUserByToken(userToken)
+      user.isDisabled = true
+      user = users[user.arrayPosition]
+      res.status(200).send('Account Disabled!');
+      writeToLog(`User "${userToken}" Was disabled By "${hash}"`);
+    }else{
+      res.status(404).send('Account Not Found');
+    }
+  } else {
+    // Authentication failed
+    res.status(401).send('Unauthorized');
+  }
+});
 
 application.post('/api/user/create', function(req, res) {
   console.log("Received user creation!");
@@ -384,7 +495,7 @@ application.post('/api/user/create', function(req, res) {
   res.status(200).send({ data: newUserUuid + "." + users.length });
   users[users.length] = user
   userCount = users.length;
-  console.log(users)
+  writeToLog(`New account was made. Data: ${JSON.stringify(user)}`);
 });
 
 application.post('/api/machine/create', function(req, res) {
@@ -395,11 +506,12 @@ application.post('/api/machine/create', function(req, res) {
     var machine = {
       token: newMachineUuid + "." + "M",
       machineType: machineType,
+      isDisabled: false,
       arrayPosition: machines.length
     };  
     res.status(200).send({ data: machine });
     machines[machines.length] = machine
-    console.log(machines)
+    writeToLog(`New machine was paired. Data: ${JSON.stringify(machine)}`);
   }else{
     res.status(403).send({})
   }
@@ -416,6 +528,7 @@ application.post('/api/machine/pair', function(req, res) {
     startMachinePairing()
     machineType = data
     res.status(200).send({});
+    writeToLog(`New machine pairing request Initiated from ${hash} With Machine type of: ${machineType}`);
   }else{
     res.status(401).send("401 Unauthorized")
   }
@@ -435,14 +548,16 @@ application.post('/api/machine/blackjack/start', function(req, res) {
   if (findMachineByToken(machineToken) == null){
     res.status(401).send("401 Unauthorized");
     console.log("Cannot create blackjack game: Unauthorized")
+    writeToLog(`A blackjack game was attempted to be made from an unauthorized source, Machine Token: ${machineToken}, Player Token: ${id}`);
     return
   }
-  if (users[userData.arrayPosition].balance <= bet){
+  if (users[userData.arrayPosition].balance < bet){
     const response = {
       success: false,
       message: "Cannot create new game. Not enough balance."
     };
     console.log("Cannot create blackjack game: Insufficient balance")
+    writeToLog(`Cannot create blackjack game due to Insufficient balance, User: "${id}"`);
     res.status(400).json(response);
     return
   }
@@ -456,6 +571,7 @@ application.post('/api/machine/blackjack/start', function(req, res) {
       message: "Cannot create new game. Game already started."
     };
     console.log("Cannot create blackjack game: Game already started")
+    writeToLog(`${id} Attempted to make a game while another was already started! Origin: ${machineToken}`);
     res.status(400).json(response);
     return
   }
@@ -473,6 +589,9 @@ application.post('/api/machine/blackjack/start', function(req, res) {
     };
     activeGames[game.gameIndex] = game;
 
+    // Remove bet amount from account to protect from any tomfoolery
+    users[userData.arrayPosition].balance = users[userData.arrayPosition].balance - bet
+
     // Return game state
     const response = {
       success: true,
@@ -487,28 +606,33 @@ application.post('/api/machine/blackjack/start', function(req, res) {
     };
     console.log(response);
     res.status(200).json(response);
+    writeToLog(`Player "${id}" Successfully created a blackjack game With data: ${JSON.stringify(response)}`);
 });
 
 
-application.get('/api/data/machines', function(req, res) {
-  // Get size and position from query parameters, with default values if not provided
-  const size = parseInt(req.query.size) || 10;
-  const position = parseInt(req.query.position) || 0;
+application.get('/api/data/logs', function(req, res) {
+  const hash = req.query.hash || "";
 
-  const data = machines.slice(position, position + (size + 1)).map(machine => {
-    return {
-      token: machine.token,
-      arrayPosition: machine.arrayPosition
-    };
-  });
-
-  res.status(200).send(data);
+  // Verify hash
+  if (verifyHash(hash)) {
+    // Read the entire log.txt file
+    fs.readFile('log.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading log.txt:', err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        res.status(200).send(data);
+      }
+    });
+  } else {
+    res.status(401).send("Unauthorized");
+  }
 });
+
 
 application.get('/api/getStatus', function(req, res) {
   res.status(200).send("200 OK");
 });
-
 
 
 // Save every 2 minutes
--- back-end/public/index.js
+++ back-end/public/index.js
@@ -80,33 +80,13 @@ function changeAlertText() {
     }
 }
 
-function dashboard(){
-    window.location.href = './'
-}
-
-function users(){
-    window.location.href = './users.html'
-}
-
-function machines(){
-    window.location.href = './machines.html'
-}
-
-function payments(){
-    window.location.href = './payment.html'
-}
-
-function settings(){
-    window.location.href = './settings.html'
-}
-
 
 function refreshList(){
     console.log("Refresh of list requested.")
     if (window.location.pathname == "/users.html"){
         const userListGroup = document.getElementById('userListGroup');
         userListGroup.innerHTML = "";
-        fetch(url + "/api/data/users?size=" + Number.MAX_SAFE_INTEGER)
+        fetch(url + "/api/data/users?size=" + Number.MAX_SAFE_INTEGER + "&hash=" + sessionStorage.getItem('logintoken'))
         .then(response => response.json())
         .then(data => {
             data.forEach(element => {
@@ -115,7 +95,7 @@ function refreshList(){
                   <h4 class="px-3">${element.token}</h4>
                   <div>
                     <i class="icon-edit">
-                      <button type="button" class="btn btn-primary" id="iconButton" data-bs-toggle="modal" data-bs-target="#EditModal" onclick="startEditUser('${element.arrayPosition}')">
+                      <button type="button" class="btn btn-primary" id="iconButton" data-bs-toggle="modal" data-bs-target="#EditModal" onclick="startEditUser('${element.token}')">
                       <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -123,7 +103,7 @@ function refreshList(){
                       </button>
                     </i>
                     <i class="icon-trash mx-4">
-                      <button type="button" class="btn btn-primary" id="iconButton" data-bs-toggle="modal" data-bs-target="#DeleteModal" onclick="startDeleteUser('${element.arrayPosition}')">
+                      <button type="button" class="btn btn-primary" id="iconButton" data-bs-toggle="modal" data-bs-target="#DeleteModal" onclick="startDeleteUser('${element.token}')">
                       <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                         <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                       </svg>
@@ -140,7 +120,7 @@ function refreshList(){
         if (window.location.pathname == "/machines.html"        ){
           const MachineListGroup = document.getElementById('MachineListGroup');
           MachineListGroup.innerHTML = "";
-          fetch(url + "/api/data/machines?size=" + Number.MAX_SAFE_INTEGER)
+          fetch(url + "/api/data/machines?size=" + Number.MAX_SAFE_INTEGER + "&hash=" + sessionStorage.getItem('logintoken'))
           .then(response => response.json())
           .then(data => {
               data.forEach(element => {
@@ -283,6 +263,21 @@ function startEditUser(selectedButton){
     .catch(error => {
       console.error(error)
     });
+}
+
+function startEditMachine(selectedButton){
+  lastSelectedButton = selectedButton
+  fetch(url + "/api/machine/getMachine?id=" + selectedButton + "&hash=" + sessionStorage.getItem('logintoken'))
+  .then(response => response.json())
+  .then(data => {
+      machineFullData = data
+      console.log(data)
+      document.getElementById("machineType").value = data.machineType
+      document.getElementById("isDisabled").checked = data.isDisabled
+  })
+  .catch(error => {
+    console.error(error)
+  });
 }
 
 function startDeleteUser(selectedButton){
